debb37370821424e7cee2444d9ccd6ba0e73b811
JinDongri <dongri.jin@gientech.com>
2026-04-02 10:47:08 +0900

    ✨添加编译后的程序

--- node/module_lx02/answer.d.mts
+++ node/module_lx02/answer.d.mts
@@ -1,5 +1,8 @@
 export {};
 /**
+ * 先に必ずコンパイルしてから　node answer.mts   or    node answer.mjsで実行OK
+ *   npx tsc
+ *
  * PS D:\workspace-jdr\node\module_lx02> node answer.mts
  * [ 110, 90, 1000, 10 ]
  */
--- node/module_lx02/answer.mjs
+++ node/module_lx02/answer.mjs
@@ -2,6 +2,9 @@ import { calculation } from './function.mjs'; // ファイルは .mts でもコ�
 const answer = calculation(100, 10);
 console.log(answer);
 /**
+ * 先に必ずコンパイルしてから　node answer.mts   or    node answer.mjsで実行OK
+ *   npx tsc
+ *
  * PS D:\workspace-jdr\node\module_lx02> node answer.mts
  * [ 110, 90, 1000, 10 ]
  */
